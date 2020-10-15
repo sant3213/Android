0a57fd7e82a9501131983424cef58cca247af901
scardenas <scardenas@cidenet.com.co>
2020-10-15 07:52:36 -0500

    feat: create lab2

---
+++ LabsCM20201/app/src/main/java/com/edu/udea/compumovil/gr0120201/lab1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.edu.udea.compumovil.gr0120201.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
